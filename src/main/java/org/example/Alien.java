1d5290d6585c52c14152864bae19e6c86dfa4763
Budnikov Illia <ilyabudkov@gmail.com>
2022-09-19 13:36:24 +0300

    learning spring and training in it, finish Spring Core -IoC Chapter

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -1,12 +1,35 @@
 package org.example;
 
 public class Alien {
-    int age;
+    private int age;
+    private Computer com;
     public Alien() {
         System.out.println("Alien Object Created...");
     }
 
+    public Alien(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Age assigned");
+        this.age = age;
+    }
+
+    public Computer getCom() {
+        return com;
+    }
+
+    public void setCom(Computer com) {
+        this.com = com;
+    }
+
     public void code(){
         System.out.println("I'm coding");
+        com.compile();
     }
 }
---
+++ src/main/java/org/example/Desktop.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Desktop implements Computer{
+    public void compile(){
+        System.out.println("Code compiled in a Desktop");
+    }
+}
